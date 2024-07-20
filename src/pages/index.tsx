17a7f70b436a639783a21b4a51bea4886a92d97e
Mohamed <mohamed.laaribi.dev@gmail.com>
2024-07-20 19:05:13 +0200

    Übersicht update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,13 +31,14 @@ const Home: NextPage = () => {
                     </h1>
                 </div>
                 <Carousel slides={slides}/>
-                <p className="text-2xl text-center text-[#557118]">
-                    Sie suchen eine liebevolle und professionelle Betreuung für Ihr Kind?
-                    In der Kindertagespflege Die 5 Helden bieten wir Kinderbetreuung bis zum 3. Lebensjahr.
-                </p>
+
                 <h2 className="font-medium text-3xl text-center text-[#f26b1d] my-4">Herzlich Willkommen bei Die 5
                     Helden!</h2>
-                <p className="text-xl text-black">
+                <p className="text-2xl text-center text-[#557118] my-6">
+                    Sie suchen eine liebevolle und professionelle Betreuung für Ihr Kind?
+                    In der Kindertagespflege <strong>Die 5 Helden</strong> bieten wir Kinderbetreuung bis <strong>zum 3. Lebensjahr</strong>.
+                </p>
+                <p className="text-xl text-black my-6">
                     In großzügigen, kindgerechten Räumen mitten in Duisburg Laar biete ich Montag bis Freitag Platz für
                     bis zu fünf Kinder bis zum 3. Lebensjahr. Durch die kleine Gruppengröße kann ich mich intensiv um
                     die Entwicklung Ihres Kindes kümmern und jedes Kind individuell und spielerisch fördern. Ich
@@ -46,7 +47,7 @@ const Home: NextPage = () => {
                     Fröhlichkeit geprägt ist.
                 </p>
                 <h3 className="font-medium text-3xl text-[#f26b1d] my-4">Unsere Philosophie</h3>
-                <p className="text-xl text-black">
+                <p className="text-xl text-black my-4">
                     Wichtig sind mir klare und verständliche Strukturen und Rituale. Sie bieten Ihrem Kind eine
                     wohlfühlende Sicherheit und Stärke, sich selbst zu vertrauen. Die Zeit an der frischen Luft kommt
                     bei uns nicht zu kurz. Wir entdecken bei Wind und Wetter die zahlreichen Spielplätze in der Nähe
--- src/pages/uebersicht.tsx
+++ src/pages/uebersicht.tsx
@@ -3,19 +3,54 @@
 const Uebersicht = () => {
     return (
         <div>
-            <h1 className="text-4xl font-bold text-center ">Übersicht</h1>
+            <h1 className="font-bold text-3xl text-[#f26b1d]">Übersicht</h1>
+            <div className="h-0.5 my-4 bg-gradient-to-r from-[#F26B1D] via-[#F2CF1D] to-[#F26B1D]"></div>
+            <div className="flex justify-center">
+                <h1 className="text-5xl font-bold bg-clip-text text-transparent" style={{
+                    backgroundImage: 'linear-gradient(to right, #50D4F2 0%, #F2CF1D 25%, #F26B1D 50%, #F2911B 75%, #30D979 100%)',
+                    WebkitBackgroundClip: 'text',
+                    WebkitTextFillColor: 'transparent',
+                }}>
+                    Die 5 Helden
+                </h1>
+            </div>
+            <img src="/images/cover-übersicht.jpg" alt="Cover Übersicht"
+                 className="w-full h-96 my-8 object-cover rounded-lg"/>
             <section className="">
-                <h2 className="text-3xl font-semibold ">Räume und Umgebung</h2>
-                <p>
+                <h2 className="font-medium text-3xl text-[#f26b1d] my-4">Übersicht</h2>
+                <p className="text-xl text-black my-6">
+                    Der geregelte Tagesablauf mit Frühstück,
+                    Morgenkreis, gemeinsamen Aktivitäten,
+                    Mittagessen, Mittagsschlaf, Snacks und
+                    anderen wiederkehrenden Ritualen bietet
+                    den Kindern Halt und Geborgenheit.
+                    Sie können dadurch schneller neue Situationen
+                    und Umgebungen erkennen und wahrnehmen
+                    und bewältigen, was neues Selbstvertrauen
+                    und Selbstwertgefühl schafft. Durch Spielen,
+                    Singen, Basteln, Toben, Tanzen, Spazierengehen
+                    und andere attraktive Aktivitäten werden Sinne
+                    und Motorik der (Klein-)Kinder gefördert.
+                    Die Wünsche der Kinder werden selbstverständlich
+                    berücksichtigt.
+                </p>
+                <img src="/images/logo.jpg" alt="Räume und Umgebung"
+                     className="w-56 h-42 my-8 object-cover rounded-lg"/>
+            </section>
+            <section className="">
+                <h2 className="font-medium text-3xl text-[#f26b1d] my-4">Räume und Umgebung</h2>
+                <p className="text-xl text-black my-6">
                     Unsere Räumlichkeiten sind großzügig und kindgerecht gestaltet. Klare und verständliche
                     Strukturen und Rituale bieten Ihrem Kind eine wohltuende Sicherheit und stärken das
                     Selbstvertrauen. Die Zeit an der frischen Luft kommt bei uns nicht zu kurz. Wir entdecken
                     bei Wind und Wetter die zahlreichen Spielplätze in der Nähe oder machen Ausflüge.
                 </p>
+                <img src="/images/logo.jpg" alt="Räume und Umgebung"
+                     className="w-56 h-42 my-8 object-cover rounded-lg"/>
             </section>
             <section className="">
-                <h2 className="text-3xl font-semibold ">Gesundheit und Ernährung</h2>
-                <p>
+                <h2 className="font-medium text-3xl text-[#f26b1d] my-4">Gesundheit und Ernährung</h2>
+                <p className="text-xl text-black my-6">
                     Die richtige Ernährung ist ein Grundstein, um gesund zu bleiben und sich fit zu fühlen.
                     Ich achte sehr darauf, dass ich für die Kinder abwechslungsreiche und gesunde Mahlzeiten
                     zubereite. Die Qualität und Herkunft der Lebensmittel spielt für mich eine große Rolle.
@@ -23,6 +58,8 @@ const Uebersicht = () => {
                     Trinken ist sehr wichtig und wird natürlich auch nicht vergessen. Wasser steht immer zur
                     Verfügung.
                 </p>
+                <img src="/images/Gesundheit und Ernährung.jpg" alt="Gesundheit und Ernährung"
+                     className="w-56 h-42 my-8 object-cover rounded-lg"/>
             </section>
         </div>
     );
